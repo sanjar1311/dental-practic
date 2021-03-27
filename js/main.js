f3d99d00e0e2f2993f44fd6028055d2373475ea1
Sanjar1311 <sanjarabduxamidov1311@gmail.com>
2021-03-28 01:16:12 +0500

    add js to slider

--- js/main.js
+++ js/main.js
@@ -1,14 +1,38 @@
-var elBody = document.querySelector('body');
-var toggler = document.querySelector('.burger__btn');
-var siteNavOpen = document.querySelector('.site-header__bottom');
-
+let elBody = document.querySelector('body');
+let toggler = document.querySelector('.burger__btn');
+let siteNavOpen = document.querySelector('.site-header__bottom');
+let preevBtn = document.querySelector('.slider__btn--preev');
+let nextBtn = document.querySelector('.slider__btn--next');
+let sliderItem = document.querySelectorAll('.slider__item');
 elBody.classList.remove('nojs');
+
 
 toggler.addEventListener('click', function() {
   siteNavOpen.classList.toggle('sitenav-open');
 });
 
-
 toggler.addEventListener ('click', function(){
   toggler.classList.toggle('burger__btn--close');
 });
+
+
+let index = 0;
+
+document.documentElement.style.setProperty("--index", index)
+
+nextBtn.addEventListener('click', function() {
+  if(index <= 1) {
+    index = index + 1
+  }
+  sliderItem.forEach(item=> {
+    item.style.transform=`translateX(calc(${index * -200}px))`;
+  })
+})
+
+preevBtn.addEventListener('click', function() {
+  if(index >= -1)
+  index = index - 1
+  sliderItem.forEach(item=> {
+    item.style.transform = `translateX(calc(${index * -200}px))`;
+  })
+})
